14433548ed9e1fc423307bcb94a0b46c02bb4948
Matthieu Stadelmann <stadelmannmatthieu@gmail.com>
2025-01-16 23:30:39 +0100

    Change PageTitleBar border color

--- src/components/PageTitleBar.vue
+++ src/components/PageTitleBar.vue
@@ -3,7 +3,7 @@ import ConnectionStatus from "./UI/ConnectionStatus.vue";
 </script>
 
 <template>
-  <div class="flex flex-col-reverse border-b-2 border-primary py-10 lg:flex-row lg:justify-between">
+  <div class="flex flex-col-reverse border-b-2 border-gray-200 py-10 lg:flex-row lg:justify-between">
     <h1 class="mt-4 text-4xl font-bold lg:mt-0">
       Trucking Web App
     </h1>
